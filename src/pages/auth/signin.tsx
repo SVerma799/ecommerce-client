d916914f142d85328430ba8d360a684d675029d6
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-07-09 23:42:40 -0700

    Changes done to add the custom sign In page

---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface SignInProps {}
+
+const SignIn: FC<SignInProps> = ({}) => {
+  return (
+    <div
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      SignIn
+    </div>
+  );
+};
+
+export default SignIn;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>
         Welcome to the Client Side of the Ecommerce of the Ecommerce Project
       </h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href="/auth/signin">Sign In</Link>
+      </button>
     </main>
   );
 }
