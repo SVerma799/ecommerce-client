0064a16b1ad47e15260c2606256516ecc1b97816
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-07-10 22:36:41 -0700

    Changes done to Navbar to the components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1>
-        Welcome to the Client Side of the Ecommerce of the Ecommerce Project
-      </h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        <Link href="/auth/signin">Sign In</Link>
-      </button>
-    </main>
+    <div>
+      <Navbar />
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <h1>
+          Welcome to the Client Side of the Ecommerce of the Ecommerce Project
+        </h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/auth/signin">Sign In</Link>
+        </button>
+      </main>
+    </div>
   );
 }
