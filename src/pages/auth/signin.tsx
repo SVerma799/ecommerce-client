5bbf1550c3eb764fc4b06d70973305bd8032980f
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-07-11 17:28:40 -0700

    Changes correct the Navbar

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,13 +1,39 @@
 import { FC } from "react";
+import { BsGithub, BsGoogle, BsFacebook } from "react-icons/bs";
+import { signIn, useSession } from "next-auth/react";
 
-interface SignInProps {}
+const SignIn = () => {
+  const provider = [
+    {
+      name: "Google",
+      Icon: BsGoogle,
+    },
+    {
+      name: "Facebook",
+      Icon: BsFacebook,
+    },
+    {
+      name: "Github",
+      Icon: BsGithub,
+    },
+  ];
 
-const SignIn: FC<SignInProps> = ({}) => {
   return (
-    <div
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      SignIn
+    <div className="flex font-signika flex-col items-center justify-center h-screen ">
+      <div className="flex flex-col space-y-2 border-2 border-grey-900 p-10 rounded-md border-indigo-800">
+        <h1 className="text-3xl font-semibold text-center ">Sign In</h1>
+        <p className="text-center ">With</p>
+        {provider.map(({ name, Icon }) => (
+          <button
+            key={name}
+            className="flex items-center justify-center px-4 py-2 space-x-2 text-white bg-indigo-800 rounded-md mb-8"
+            onClick={() => signIn(name.toLowerCase())}
+          >
+            <Icon />
+            <span>Sign in with {name}</span>
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white dark:bg-gray-950">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center">
           <Image
@@ -46,11 +46,11 @@ const Navbar = () => {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-950 md:dark:bg-gray-950 dark:border-gray-790">
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className="block py-2 pl-3 pr-4 text-white bg-indigo-800 rounded md:bg-transparent md:text-indigo-700 md:p-0 dark:text-white md:dark:text-indigo-500"
                 aria-current="page"
               >
                 Home
@@ -61,7 +61,7 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-700 md:p-0 dark:text-white md:dark:hover:text-indigo-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Products
               </a>
@@ -70,7 +70,7 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-700 md:p-0 dark:text-white md:dark:hover:text-indigo-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Categories
               </a>
@@ -80,7 +80,7 @@ const Navbar = () => {
               <button
                 id="dropdownNavbarLink"
                 data-dropdown-toggle="dropdownNavbar"
-                className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-indigo-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                 onClick={() => setIsOpen(!isOpen)}
               >
                 <BiUserCircle className="h-7 w-7" />
@@ -89,7 +89,7 @@ const Navbar = () => {
                 id="dropdownNavbar"
                 className={cn({
                   hidden: !isOpen,
-                  "z-1000000 absolute mt-5  font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600":
+                  "z-100 absolute mt-5  font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-36 dark:bg-gray-900 dark:divide-gray-600":
                     true,
                 })}
               >
@@ -100,7 +100,7 @@ const Navbar = () => {
                   <li>
                     <Link
                       href="auth/signin"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white"
                     >
                       SignIn
                     </Link>
